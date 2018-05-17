545b9eb51cee80bfb1465399532a4078926ca391
QuantumWubs <stormrulez5@gmail.com>
2018-05-17 21:29:46 +1000

    Tranaction menu Work

--- Transact.py
+++ Transact.py
@@ -3,10 +3,24 @@
     # Get Location of Partner Node
     # Connect to Partner Node
         #Make Check to make sure Shared Key is the same (Cipher)
+
     # Specify who is the Recipient
 # TODO: (IF this Client is Payer): Get Specified amount (To Transact)
-
-while(ACTION == 'T'):
+ACTION = input('>').upper()
+if(ACTION == 'T'):
     print('========[ Transact ]========')
-    Partner = input('Input Partner ID')
-    TransAction = input('Action Send(S)/Receive(R)/Cancel(X)\n>')
+    while(ACTION == 'T'):
+        Partner = input('\nInput Partner ID\n>')
+        TransAction = input('\nAction Send(S)/Receive(R)/Cancel(X)\n>').upper()
+
+        while(TransAction == 'S'):
+            # TODO: Send Token
+            print('Send Code')
+
+        while(TransAction == 'R'):
+            # TODO: Receive Token
+            print('Receive Code')
+
+        if(TransAction == 'X'):
+            if(input('Are you sure? (Y/N)').upper() == 'Y'):
+                break
