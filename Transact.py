1b0baff8e39a77fc1310533d1cfa5b25fbe27662
QuantumWubs <stormrulez5@gmail.com>
2018-05-17 21:19:48 +1000

    starting prompts for transaction

--- Transact.py
+++ Transact.py
@@ -1,10 +1,13 @@
+# TODO: Make P2P connection with Transaction Partner
+    # Create Node
+    # Get Location of Partner Node
+    # Connect to Partner Node
+        #Make Check to make sure Shared Key is the same (Cipher)
+    # Specify who is the Recipient
+# TODO: (IF this Client is Payer): Get Specified amount (To Transact)
+
 while(ACTION == 'T'):
     print('========[ Transact ]========')
-
-    # TODO: Make P2P connection with Transaction Partner
-        # Create Node
-        # Get Location of Partner Node
-        # Connect to Partner Node
-            #Make Check to make sure Shared Key is the same (Cipher)
-        # Specify who is the Recipient
-    # TODO: (IF this Client is Payer): Get Specified amount (To Transact)
+    Partner = input('Input Partner ID')
+    TransAction = input('Action Send(S)/Receive(R)/Cancel(X)\n>')
+    
