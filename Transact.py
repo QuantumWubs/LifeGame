68e7c1a58c9047401aea97b1536fdf894f4e1764
QuantumWubs <stormrulez5@gmail.com>
2018-05-17 20:19:39 +1000

    Todo List for Transaction Code

---
+++ Transact.py
@@ -0,0 +1,10 @@
+while(ACTION == 'T'):
+    print('========[ Transact ]========')
+
+    # TODO: Make P2P connection with Transaction Partner
+        # Create Node
+        # Get Location of Partner Node
+        # Connect to Partner Node
+            #Make Check to make sure Shared Key is the same (Cipher)
+        # Specify who is the Recipient
+    # TODO: (IF this Client is Payer): Get Specified amount (To Transact)
